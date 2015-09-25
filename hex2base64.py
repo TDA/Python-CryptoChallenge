f0d5cb63cd4c9e5bd3bbe01b8bb60fe885167a55
Sai Pc <schand31@asu.edu>
2015-09-25 12:18:03 -0700

    hex2base64 utility

---
+++ hex2base64.py
@@ -0,0 +1,10 @@
+__author__ = 'saipc'
+
+
+def hex2base64 (str):
+    return str.decode('hex').encode('base64')
+
+
+
+base64_string = hex2base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+print base64_string
