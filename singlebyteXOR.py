e645aeec64295dedebf874658db228d707642a65
Sai Pc <schand31@asu.edu>
2015-09-25 13:02:59 -0700

    wrote the single byte in two ways, one with the fixedXOR and one with a loop

--- singlebyteXOR.py
+++ singlebyteXOR.py
@@ -1,18 +1,28 @@
 __author__ = 'saipc'
 
 import string
+from fixedXOR import fixedXOR
 
 def singleByteXOR(a):
     # decode from hex
-    a = a.decode('hex')
+    a_decoded = a.decode('hex')
     # convert to bytes
-    a_bytearray = bytearray(a)
+    a_bytearray = bytearray(a_decoded)
     letters = [x for x in string.ascii_letters]
-    numbers = [x for x in xrange(0,10)]
-    for key in letters + numbers:
-        print key
+    c_bytearray = bytearray(len(a_bytearray))
+    for key in letters:
+        new_key = int(ord(key))
+        for x in xrange(0, len(a_bytearray)):
+            c_bytearray[x] = a_bytearray[x] ^ new_key
+        #print c_bytearray, chr(new_key)
+
+        # or can reuse the old pgm
+        new_key2 = key * len(a_bytearray)
+        print fixedXOR(a, new_key2.encode('hex')).decode('hex')
+
+
 
 
 if __name__ == '__main__':
-    xor = singleByteXOR('1c0111001f010100061a024b53535009181c')
-    print xor
+    xor = singleByteXOR('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+
