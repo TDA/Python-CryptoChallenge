4bd6c49ece6ce02fdc058c0fb96559d2b8749659
Sai Pc <schand31@asu.edu>
2015-09-25 12:32:52 -0700

    fixed XOR with hex to bytearray conversion and back added comments

---
+++ fixedXOR.py
@@ -0,0 +1,22 @@
+__author__ = 'saipc'
+
+
+def fixedXOR(a, b):
+    # decode from hex
+    a = a.decode('hex')
+    b = b.decode('hex')
+    # convert to bytes
+    a_bytearray = bytearray(a)
+    b_byteaaray = bytearray(b)
+    c_bytearray = bytearray(len(a_bytearray))
+    # fixed length buffers, so enough to check one length
+    for x in xrange(0, len(a)):
+        # xor
+        c_bytearray[x] = a_bytearray[x] ^ b_byteaaray[x]
+    # decode bytes to string, and then encode back to hex
+    c = (c_bytearray).decode('utf-8').encode('hex')
+    return c
+
+if __name__ == '__main__':
+    xor = fixedXOR('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
+    print xor
--- hex2base64.py
+++ hex2base64.py
@@ -4,7 +4,6 @@ __author__ = 'saipc'
 def hex2base64 (str):
     return str.decode('hex').encode('base64')
 
-
-
-base64_string = hex2base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
-print base64_string
+if __name__ == '__main__':
+    base64_string = hex2base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+    print base64_string
