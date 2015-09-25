41e97aefaf2deb4bec2c0784811bf5559b65fa37
Sai Pc <schand31@asu.edu>
2015-09-25 14:32:31 -0700

    Added comments and removed redundant stuff

--- singlebyteXOR.py
+++ singlebyteXOR.py
@@ -5,25 +5,31 @@ from fixedXOR import fixedXOR
 
 
 def calculateFrequency(str, key):
+    # the most frequent letters in the alphabet
     freq_map = {x: 0 for x in "etaoinshrdlu"}
     for char in str:
         if freq_map.has_key(char):
+            # update occurrence
             freq_map[char] = freq_map[char] + 1
     sum = 0
+    # return a count of all the letters
     for char in freq_map.keys():
         sum += freq_map[char]
     return sum
 
 def singleByteXOR(a):
-    a_bytearray = bytearray(a.decode('hex'))
+    length = len(a)
     letters = [x for x in string.ascii_letters]
     freq_of_letters = []
     for key in letters:
         # or can reuse the old pgm, much cleaner :)
-        new_key2 = key * len(a_bytearray)
-        c = fixedXOR(a, new_key2.encode('hex')).decode('hex')
+        # encode the key and expand it to the length
+        # of the string
+        new_key2 = key.encode('hex') * length
+        c = fixedXOR(a, new_key2)\
+            .decode('hex')
         freq_of_letters.append(calculateFrequency(c, new_key2))
-
+    # find the decoded string that is most similar to english
     max_match = max(freq_of_letters)
     return letters[freq_of_letters.index(max_match)]
 
