76ecd6b074d35d12e65b29b22530b01e8d3473e6
Sai Pc <schand31@asu.edu>
2015-09-25 13:05:17 -0700

    Refactored to reduce code

--- singlebyteXOR.py
+++ singlebyteXOR.py
@@ -4,19 +4,10 @@ import string
 from fixedXOR import fixedXOR
 
 def singleByteXOR(a):
-    # decode from hex
-    a_decoded = a.decode('hex')
-    # convert to bytes
-    a_bytearray = bytearray(a_decoded)
+    a_bytearray = bytearray(a.decode('hex'))
     letters = [x for x in string.ascii_letters]
-    c_bytearray = bytearray(len(a_bytearray))
     for key in letters:
-        new_key = int(ord(key))
-        for x in xrange(0, len(a_bytearray)):
-            c_bytearray[x] = a_bytearray[x] ^ new_key
-        #print c_bytearray, chr(new_key)
-
-        # or can reuse the old pgm
+        # or can reuse the old pgm, much more pythonic :)
         new_key2 = key * len(a_bytearray)
         print fixedXOR(a, new_key2.encode('hex')).decode('hex')
 
